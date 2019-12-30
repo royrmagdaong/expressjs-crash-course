6a6669646adfbf1e25522a951ed1d1610cfde1b8
roy <royrmagdaong@gmail.com>
2019-12-30 13:48:39 +0800

    initial commit

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        id:1,
+        name: "Sponge Bob",
+        email: "spongebob@gmail.com",
+        status: "active"
+    },
+    {
+        id:2,
+        name: "Patick Star",
+        email: "patrickstar@gmail.com",
+        status: "active"
+    },
+    {
+        id:3,
+        name: "Squidward Tentacles",
+        email: "tentacles@gmail.com",
+        status: "active"
+    },
+    {
+        id:4,
+        name: "Mr Crab",
+        email: "KrustyKrab@gmail.com",
+        status: "active"
+    },
+]
+
+module.exports = members;
